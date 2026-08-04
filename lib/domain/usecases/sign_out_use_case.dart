4c8fa60b95d0648f1895fc9ee9460306be2d675c
Justin.dev <justinbina515@gmail.com>
2026-08-04 15:39:13 +0000

    Add SignOut use case

---
+++ lib/domain/usecases/sign_out_use_case.dart
@@ -0,0 +1,7 @@
+import '../../domain/repositories/auth_repository.dart';
+
+class SignOutUseCase {
+  final AuthRepository _repository;
+  SignOutUseCase(this._repository);
+  Future<void> call() async => await _repository.signOut();
+}
