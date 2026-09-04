83fe7468d8e42895c3c01261c38b5cee45eaeeba
Justin.dev <justinbina515@gmail.com>
2026-09-04 16:36:34 +0000

    fix: align navigation integration imports with package declaration

--- integration_test/news_flow_test.dart
+++ integration_test/news_flow_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:flutter_clean_api_app/main.dart' as app;
+import 'package:ecommerce_project/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
